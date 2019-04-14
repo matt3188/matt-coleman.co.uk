b685cf3b70310d9bb75b59eea298acb5b8c988b7
Matt Coleman <matt.coleman@protonmail.com>
2019-04-14 17:35:48 +0100

    Added the 3 icons to App.js and added CSS for hover states

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 
 import Header from './components/Header';
+import { CodepenIcon, EmailIcon, GithubIcon } from './components/icons';
 
 class App extends Component {
   render() {
@@ -31,6 +32,25 @@ class App extends Component {
         />
         <div className="row">
           <Header />
+
+          <div className="col-md-12">
+            <ul className="contact-list">
+              <li>
+                <a className="btn" href="https://github.com/matt3188" target="_blank" rel="noopener noreferrer">
+                  <GithubIcon fill="#fff" width={50} height={50} />
+                </a>
+              </li>
+              <li>
+                <a className="btn" href="http://codepen.io/mattcoleman" target="_blank" rel="noopener noreferrer">
+                  <CodepenIcon fill="#fff" width={50} height={50} />
+                </a>
+              </li>
+              <li>
+                <a className="btn" href="mailto:hello@matt-coleman.co.uk">
+                  <EmailIcon fill="#fff" width={50} height={50} />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
