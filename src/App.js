769f21e2d82cd7a44d88c4ac2d9e82ea089c56b2
Matt Coleman <matt.coleman@protonmail.com>
2019-04-14 17:33:34 +0100

    Added a particle background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
 import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Particles
+          className="particles"
+          params={{
+            particles: {
+              number: {
+                value: 50
+              },
+              size: {
+                value: 3
+              }
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse'
+                }
+              }
+            }
+          }}
+          style={{ position: 'fixed', width: '100vw', height: '100vh', left: 0, top: 0 }}
+        />
         <div className="row">
           <Header />
           </div>
