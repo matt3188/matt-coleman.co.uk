538f0c0dc06c065c1fc65f765cb640faabc47cbd
Matt Coleman <matt.coleman@protonmail.com>
2019-04-27 21:44:27 +0100

    Added Skill to CV page

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Skill from '../components/Skill';
 
 const Cv = () => (
   <div>
+    <Skill label="HTML" percentage={90} delay={500} />
+    <Skill label="CSS" percentage={80} delay={500} />
+    <Skill label="Javascript" percentage={70} delay={750} />
   </div>
 );
 
