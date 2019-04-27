2787c9aaae966da8fc007aa8fe38d2e430bf3b12
Matt Coleman <matt.coleman@protonmail.com>
2019-04-27 21:41:35 +0100

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
-  <nav class="nav">
+  <nav className="nav">
     <ul className="horizontal-list">
       <li>
         <NavLink activeClassName="is-active" exact={true} to="/">
