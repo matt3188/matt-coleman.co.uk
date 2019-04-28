0c0ff799b9059373ca762a281074d5f8183edf79
Matt Coleman <matt.coleman@protonmail.com>
2019-04-28 12:16:55 +0100

    Created mlobile navigation trigger

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = () => (
-  <nav className="nav">
-    <ul className="horizontal-list">
-      <li>
-        <NavLink activeClassName="is-active" exact={true} to="/">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink activeClassName="is-active" to="/my-cv">
-          My CV
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+class Navigation extends React.Component {
+  state = {
+    navOpen: false
+  };
 
+  clickHandler = () => {
+    this.setState({
+      navOpen: !this.state.navOpen
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button
+          className={`nav__mobile-menu-toggle ${this.state.navOpen ? 'is-active' : ''}`}
+          onClick={this.clickHandler}
+        />
+        <nav className={`nav ${this.state.navOpen ? 'is-active' : ''}`}>
+          <ul className="horizontal-list">
+            <li>
+              <NavLink onClick={this.clickHandler} activeClassName="is-active" exact={true} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink onClick={this.clickHandler} activeClassName="is-active" to="/my-cv">
+                My CV
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
+}
 export default Navigation;
