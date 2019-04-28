fd658af99a6582e336131b0c543dc3d38d2f7b7a
Matt Coleman <matt.coleman@protonmail.com>
2019-04-28 12:16:27 +0100

    Incremented transition delay for skills

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -4,8 +4,8 @@ import Skill from './/Skill';
 const Cv = () => (
   <div>
     <Skill label="HTML" percentage={90} delay={500} />
-    <Skill label="CSS" percentage={80} delay={500} />
-    <Skill label="Javascript" percentage={70} delay={750} />
+    <Skill label="CSS" percentage={80} delay={750} />
+    <Skill label="Javascript" percentage={70} delay={1000} />
   </div>
 );
 
