c31aea957560603cf517fc9ba313c99542136545
Matt Coleman <matt.coleman@protonmail.com>
2019-05-03 01:14:54 +0100

    Created routes folder for page components

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,26 +2,24 @@ import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-import Home from './Home';
-import Cv from './Cv';
-import NotFoundPage from './NotFoundPage';
+import Home from 'routes/Home';
+import Cv from 'routes/Cv';
+import NotFound from 'routes/NotFound';
 
-const Container = ({ location }) => {
-  return (
-    <div className="container">
-      <TransitionGroup className="transition-group">
-        <CSSTransition key={location.key} timeout={{ enter: 300, exit: 300 }} classNames={'fade'}>
-          <section className="route-section">
-            <Switch location={location}>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/my-cv" component={Cv} />
-              <Route component={NotFoundPage} />
-            </Switch>
-          </section>
-        </CSSTransition>
-      </TransitionGroup>
-    </div>
-  );
-};
+const Container = ({ location }) => (
+  <div className="container">
+    <TransitionGroup className="transition-group">
+      <CSSTransition key={location.key} timeout={{ enter: 300, exit: 300 }} classNames={'fade'}>
+        <section className="route-section">
+          <Switch location={location}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/my-cv" component={Cv} />
+            <Route component={NotFound} />
+          </Switch>
+        </section>
+      </CSSTransition>
+    </TransitionGroup>
+  </div>
+);
 
 export default withRouter(Container);
