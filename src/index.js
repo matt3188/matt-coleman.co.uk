0e01544317987cac06a5e4aa723934cde529806b
Matt Coleman <matt.coleman@protonmail.com>
2019-05-03 01:15:10 +0100

    Moved service worker into utils folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import 'scss/index.scss';
 import App from 'App';
+import * as serviceWorker from 'utils/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
