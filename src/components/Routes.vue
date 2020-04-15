81e41436147ccdba06c06afa897ea55811b532b9
Matt Coleman <matt.coleman@protonmail.com>
2020-04-15 21:50:53 +0100

    Added a Routes component to loop through all Vue routes from Router

---
+++ src/components/Routes.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="router">
+    <ul class="router__list">
+      <li v-for="(route, index) in routes" v-bind:key="index">
+        <router-link class="router__link" :to="route.path">{{ route.name }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Routes',
+  beforeMount() {
+    this.routes = this.$router.options.routes;
+  },
+};
+</script>
+
+<style lang="scss">
+.router {
+  &__list {
+    display: flex;
+    justify-content: center;
+    padding: 30px;
+
+    li {
+      list-style: none;
+    }
+  }
+
+  &__link {
+    color: #2c3e50;
+    font-weight: bold;
+    padding: 10px 20px;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
