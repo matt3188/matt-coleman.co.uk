bdf4c948adfd3a0f613e3b7597d89eb1e34b9146
Matt Coleman <matt.coleman@protonmail.com>
2020-04-16 13:33:47 +0100

    Added Jest to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,4 +20,16 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
