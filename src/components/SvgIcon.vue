851116f42f7062c79cd6dfaba1840e4349a71e8e
Matt Coleman <matt.coleman@protonmail.com>
2020-04-23 17:40:18 +0100

    Created an SVG icon component and defined the social icons needed so far

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :style="iconStyle">
+    <svg preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <g :fill="iconColor">
+        <path :d="path" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+import ICONS from '@/components/icons';
+
+export default {
+  name: 'SvgIcon',
+  props: {
+    iconType: {
+      type: String,
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor',
+    },
+
+    width: {
+      type: [Number, String],
+      default: 18,
+    },
+    height: {
+      type: [Number, String],
+      default: 18,
+    },
+  },
+  computed: {
+    iconStyle() {
+      const { width, height } = this;
+      return {
+        width: `${width}px`,
+        height: `${height}px`,
+      };
+    },
+    path() {
+      return ICONS[this.iconType];
+    },
+  },
+};
+</script>
