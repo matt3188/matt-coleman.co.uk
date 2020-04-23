6359afb78794fc3ed3d5e603e2f1cf2f687b940a
Matt Coleman <matt.coleman@protonmail.com>
2020-04-23 22:18:07 +0100

    Created a Skill bar component

---
+++ src/components/SkillBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="skill-bar-container">
+    <div class="skill-labels">
+      <span class="title">{{ title }}</span
+      ><span class="skill-level">{{ skillLevel() }}</span>
+    </div>
+    <div class="skill-bar">
+      <div
+        class="skill-bar-progress"
+        :style="`width: ${skillLevel()}; background-color: ${color}`"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkillBar',
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+    level: {
+      required: true,
+      type: Number,
+    },
+    color: {
+      required: true,
+      type: String,
+    },
+  },
+  methods: {
+    skillLevel() {
+      return `${parseInt(this.$props.level, 10)}%`;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.skill-bar-container {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 20px;
+}
+.skill-labels {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 5px;
+}
+.title {
+  font-weight: 500;
+}
+.skill-bar {
+  background-color: $color-light-grey;
+  border-radius: 10px;
+}
+.skill-bar-progress {
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,90 @@
 <template>
   <div class="about">
-    <p>This page is about me</p>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4"><Avatar :fill="avatarBg" width="200px" /></div>
+        <div class="col-md-8">
+          <div class="about-me">
+            <p>
+              I'm Matt Coleman, I'm a strong and passionate front-end developer with good
+              communication skills and a keen eye for detail. My philosophy is, if something is
+              worth doing, it's worth doing right.
+            </p>
+            <p>
+              I have a background in design, which allows me to bridge the gap between design and
+              development within a team. I like to create modular, portable code with a DRY approach
+              and I also try and future proof my code as best I can.
+            </p>
+            <button class="btn btn__standard">Download CV</button>
+          </div>
+        </div>
+      </div>
+      <SkillsList :skills="skills" />
+    </div>
   </div>
 </template>
+
+<script>
+import variables from '@/assets/scss/_variables.scss';
+import Avatar from '@/components/Avatar.vue';
+import SkillsList from '@/components/SkillsList.vue';
+
+export default {
+  name: 'About',
+  components: {
+    SkillsList,
+    Avatar,
+  },
+  data() {
+    return {
+      skills: [],
+    };
+  },
+  mounted() {
+    this.skills = [
+      { name: 'HTML', level: 90, colour: variables.yellow },
+      { name: 'CSS', level: 90, colour: variables.coralRed },
+      { name: 'Javascript', level: 80, colour: variables.purple },
+    ];
+  },
+  computed: {
+    avatarBg() {
+      return variables.aubergine;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.about-me {
+  background-color: $color-white;
+  border-radius: 15px;
+  box-shadow: 0 5px 20px 0 rgba(69, 67, 96, 0.1);
+  margin-bottom: 50px;
+  padding: 25px;
+  position: relative;
+  text-align: left;
+
+  @include media-breakpoint-up(md) {
+    &:before {
+      border-bottom: 10px solid transparent;
+      border-right: 15px solid #fff;
+      border-top: 10px solid transparent;
+      content: '';
+      height: 0;
+      position: absolute;
+      right: 100%;
+      top: 20%;
+      width: 0;
+    }
+  }
+}
+
+.list {
+  &--vertical {
+    li {
+      margin: 0 0 10px;
+    }
+  }
+}
+</style>
