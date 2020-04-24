dcce28a5b78e0270ab8de85ca608ee0677a3a72c
Matt Coleman <matt.coleman@protonmail.com>
2020-04-24 23:45:48 +0100

    Created an Input component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="form-group">
+    <label :for="id">{{ label }}</label>
+    <input v-if="type !== 'textarea'" :id="id" class="form-control" :type="type" :name="name" />
+    <textarea v-else :id="id" class="form-control" :type="type" :name="name" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  props: {
+    label: String,
+    id: String,
+    type: String,
+    name: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.form {
+  &-group {
+    display: flex;
+  }
+}
+</style>
