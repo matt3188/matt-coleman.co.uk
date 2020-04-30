bc3613eb8d9e6f252cc52fc228d2a3b9bd4d1edc
Matt Coleman <matt.coleman@protonmail.com>
2020-04-30 18:35:44 +0100

    Created an Experience component and moved the styles into it

---
+++ src/components/Experience.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="experience">
+    <small class="date">{{ date }}</small>
+    <h3 class="role">{{ role }}</h3>
+    <span class="company">{{ company }}</span>
+    <div>{{ description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Experience',
+  props: {
+    date: String,
+    role: String,
+    company: String,
+    description: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.experience {
+  border-left: 1px solid $color-coral-red;
+  padding: 0 0 25px 25px;
+  position: relative;
+  padding-left: 20px;
+
+  &:before {
+    background-color: $color-coral-red;
+    border-radius: 50%;
+    content: '';
+    height: 11px;
+    position: absolute;
+    left: -6px;
+    outline: 6px solid #fff;
+    top: 4px;
+    width: 11px;
+  }
+
+  .date,
+  .company {
+    display: block;
+  }
+
+  .date {
+    color: #8b88b1;
+    font-size: 14px;
+  }
+  .role {
+    font-size: 20px;
+    margin: 10px 0;
+  }
+  .description {
+    margin: 0;
+  }
+}
+</style>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,52 +1,62 @@
 <template>
   <ul class="list horizontal--list">
-    <li class="experience" v-for="exp in experiences" :key="exp.id">
-      <small class="date">{{ exp.date }}</small>
-      <h3 class="job-title">{{ exp.jobTitle }}</h3>
-      <span class="company">{{ exp.company }}</span>
-      {{ exp.description }}
+    <li v-for="exp in experiences" :key="exp.id">
+      <Experience v-bind="exp" />
     </li>
   </ul>
 </template>
 
 <script>
+import Experience from '@/components/Experience.vue';
+
 export default {
   name: 'Timeline',
+  components: { Experience },
   data: () => ({
     experiences: [
       {
         id: 0,
-        date: 'Feb 2019 - Present',
-        jobTitle: 'Lead Front-end Developer',
-        company: 'Compnay',
+        date: 'May 2019 - Present',
+        role: 'Lead Front-end Developer',
+        company: 'Radley Yeldar',
         description: 'Lorem ipsum',
       },
       {
         id: 1,
-        date: '',
-        jobTitle: 'Senior Front-end Developer',
-        company: 'Compnay',
+        date: 'Feb 2019 - May 2019',
+        role: 'Senior Front-end Developer',
+        company: 'Radley Yeldar',
+        description: 'Lorem ipsum',
+      },
+      {
+        id: 2,
+        date: 'Feb 2018 - Feb 2019',
+        role: 'Senior Client-side Engineer',
+        company: 'Dare',
+        description: 'Lorem ipsum',
+      },
+      {
+        id: 3,
+        date: 'May 2017 - Feb 2018',
+        role: 'UI Developer',
+        company: 'Spindrift',
+        description: 'Lorem ipsum',
+      },
+      {
+        id: 4,
+        date: 'Sept 2015 - Apr 2017',
+        role: 'Front-end Developer',
+        company: 'Dennis Publishing',
+        description: 'Lorem ipsum',
+      },
+      {
+        id: 5,
+        date: 'Aug 2014 - Aug 2015',
+        role: 'Digital Designer',
+        company: 'Adnostic',
         description: 'Lorem ipsum',
       },
     ],
   }),
 };
 </script>
-
-<style lang="scss">
-.experience {
-  margin: 0 0 25px;
-
-  .date {
-    color: #8b88b1;
-    font-size: 14px;
-  }
-  .job-title {
-    font-size: 20px;
-    margin: 10px 0;
-  }
-  .description {
-    margin: 0;
-  }
-}
-</style>
