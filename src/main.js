8a996f3d88452ea93315540c4a0b5c53924d1ecc
Matt Coleman <matt.coleman@protonmail.com>
2020-05-01 17:02:17 +0100

    Added a setViewport function and installed lodash to throttle the windowsize

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueTypedJs from 'vue-typed-js';
+import { throttle } from 'lodash';
 
 import App from './App.vue';
 import './registerServiceWorker';
@@ -10,9 +11,19 @@ Vue.use(VueTypedJs);
 
 Vue.config.productionTip = false;
 
-new Vue({
+// eslint-disable-next-line
+const vm = new Vue({
   router,
   VueTypedJs,
   store,
+  created() {
+    window.addEventListener('resize', throttle(this.windowResize, 200));
+    this.$store.dispatch('setViewport');
+  },
+  methods: {
+    windowResize() {
+      this.$store.dispatch('setViewport');
+    },
+  },
   render: (h) => h(App),
 }).$mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+const types = {
+  MOBILE: 'mobile',
+  TABLET: 'tablet',
+  DESKTOP: 'desktop',
+};
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    viewport: types.MOBILE,
+  },
+  mutations: {
+    SET_VIEWPORT(state, width) {
+      state.viewport = types.MOBILE;
+
+      // Bootstraps media breakpoints
+      if (width >= 768) state.viewport = types.TABLET;
+      if (width >= 1200) state.viewport = types.DESKTOP;
+    },
+  },
+  actions: {
+    setViewport({ commit }) {
+      commit(
+        'SET_VIEWPORT',
+        window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+      );
+    },
+  },
   modules: {},
 });
