9ca98ce764ec523818faca82b1fa282a7c6b550f
Matt Coleman <matt.coleman@protonmail.com>
2020-05-01 17:00:09 +0100

    Added descriptions to each role

--- src/views/About.vue
+++ src/views/About.vue
@@ -74,42 +74,47 @@ export default {
           date: 'May 2019 - Present',
           role: 'Lead Front-end Developer',
           company: 'Radley Yeldar',
-          description: 'Lorem ipsum',
+          description:
+            'After proving myself as an integral part of the team, RY promoted me to Lead front-end.',
         },
         {
           id: 1,
           date: 'Feb 2019 - May 2019',
           role: 'Senior Front-end Developer',
           company: 'Radley Yeldar',
-          description: 'Lorem ipsum',
+          description:
+            "Thrown in at the deep end on one of the world's top law firms Sitecore builds.",
         },
         {
           id: 2,
           date: 'Feb 2018 - Feb 2019',
           role: 'Senior Client-side Engineer',
           company: 'Dare',
-          description: 'Lorem ipsum',
+          description:
+            'Led the front-end on a huge digital transformation for one of Europes largest banks.',
         },
         {
           id: 3,
           date: 'May 2017 - Feb 2018',
           role: 'UI Developer',
           company: 'Spindrift',
-          description: 'Lorem ipsum',
+          description:
+            'Mostly worked on-site with their clients, helping them to grow their digital prescence.',
         },
         {
           id: 4,
           date: 'Sept 2015 - Apr 2017',
           role: 'Front-end Developer',
           company: 'Dennis Publishing',
-          description: 'Lorem ipsum',
+          description:
+            'Worked on their entire range of in-house magazines that have supporting websites.',
         },
         {
           id: 5,
           date: 'Aug 2014 - Aug 2015',
           role: 'Digital Designer',
           company: 'Adnostic',
-          description: 'Lorem ipsum',
+          description: 'Designing and building interactive adverts for a plethora of clients.',
         },
       ],
       publicPath: process.env.BASE_URL,
