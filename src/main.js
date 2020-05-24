708b17befd1293657ba5261aa6577a3d87e6979b
Matt Coleman <matt.coleman@protonmail.com>
2020-05-24 17:00:01 +0100

    Removed unnecessary global usage of Vue Typed JS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import VueTypedJs from 'vue-typed-js';
 import { throttle } from 'lodash-es';
 import { ObserveVisibility } from 'vue-observe-visibility';
 
@@ -10,8 +9,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.use(VueTypedJs);
-
 // Directives
 Vue.directive('observe-visibility', ObserveVisibility);
 
@@ -20,7 +17,6 @@ Vue.config.productionTip = false;
 // eslint-disable-next-line
 const vm = new Vue({
   router,
-  VueTypedJs,
   store,
   created() {
     window.addEventListener('resize', throttle(this.windowResize, 200));
