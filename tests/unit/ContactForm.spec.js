9e030bbf44f89c3a38e6fee015da9688790dafb3
Matt Coleman <matt.coleman@protonmail.com>
2020-07-24 13:54:39 +0100

    Added contact form test

---
+++ tests/unit/ContactForm.spec.js
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils';
+import ContactForm from '@/components/ContactForm.vue';
+
+describe('ContactForm.vue', () => {
+  const factory = (propsData) => {
+    return mount(ContactForm, {
+      propsData: {
+        formData: {
+          submitBtn: {
+            send: 'Send',
+            submitting: 'Sending...',
+          },
+          formFields: [
+            {
+              id: 'name',
+              label: 'Your Name',
+              type: 'text',
+              name: 'name',
+              required: true,
+            },
+          ],
+          ...propsData,
+        },
+      },
+    });
+  };
+
+  it('renders props.btnLabel when passed', () => {
+    const btnLabel = 'Send';
+    const wrapper = factory();
+    const button = wrapper.find('.btn__standard');
+
+    expect(button.text()).toMatch(btnLabel);
+  });
+});
