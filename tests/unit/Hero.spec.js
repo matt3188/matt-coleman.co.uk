e7a73653fcfa9acf37381ab025446397376fa200
Matt Coleman <matt.coleman@protonmail.com>
2021-12-30 18:06:43 +0000

    Updated test and About page

--- tests/unit/Hero.spec.js
+++ tests/unit/Hero.spec.js
@@ -11,7 +11,7 @@ describe('Hero.vue', () => {
   })
 
   it('renders props.btnLabel when passed', () => {
-    const btnLabel = 'Hire me'
+    const btnLabel = 'Contact me'
     const wrapper = shallowMount(Hero, {
       propsData: { btnLabel }
     })
@@ -21,10 +21,11 @@ describe('Hero.vue', () => {
   it('props.typedList receives an array', () => {
     const expected = [
       "I'm a Front-end Developer",
-      "I'm a Cyling enthusiast",
-      "I'm a Coffee lover",
-      "I'm a collector of LEGO"
-    ]
+      "I'm a cycling enthusiast",
+      "I'm a coffee lover",
+      "I'm a collector of LEGO",
+      "I'm a motorcylce rider"
+  ]
     expect(expect.arrayContaining(expected))
   })
 })
